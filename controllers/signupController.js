9b50c108fe2ac0286c2c28ff038d9b809845bc77
Ramirez-Benmar <134592419+Ramirez-Benmar@users.noreply.github.com>
2023-08-03 23:06:10 +0800

    Adding Validations
    Moving new code, because I worked on an outdated repo

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -12,7 +12,32 @@ const signupController = {
         res.render('SignUp',res);
     },
 
+    checkSignUp: async function (req, res) {
+        const userId = req.params.idNumber
+        console.log(userId)
+        const parts = userId.split('=');
+        const finalId = parts[1]
+        console.log(typeof(finalId))
+        console.log("Final ID Number is:")
+        console.log(finalId)
+        var isExisting = await User.find({'idNumber': finalId});
+        console.log("Is Existing")
+        console.log(isExisting)
+        if (isExisting.length>0) {
+            console.log("Existing Already!")
+            res.json("error");
+        } 
+        else {
+            res.json("unique")
+        }
+    },
+
     postSignUp: async function (req, res) {
+
+        console.log("Inside PostSignUp")
+        const { firstName, lastName, email, idNumber, password, securityCode, designation, passengerType } = req.body;
+        console.log(req.body)
+
         const user = {
             firstName: req.body.user_firstName,
             lastName: req.body.user_lastName,
@@ -25,18 +50,22 @@ const signupController = {
             profilePicture: "images/profilepictures/Default.png"
         }
 
-        var result = await db.insertOne(User, user);
-
-        if( result ){
-			console.log(result);
-            console.log('User successfully added');
-            res.render('Login', {isRegistered: true});
-        }
-        else{
-            console.log('User not added');
+        var isExisting = await User.findOne({'idNumber': idNumber});
+        if (isExisting) { 
+            console.log("ID NUMBER IS IN DATABASE")
+            res.redirect("/SignUp")
+        } 
+        else {
+            var result = await db.insertOne(User, req.body);
+            if( result ){
+                console.log('User successfully added');
+                res.render("Login")
+            }
+            else{
+                console.log('User not added');
+            }
         }
     }
-
 }
 
 module.exports = signupController;
